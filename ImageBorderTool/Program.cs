6286ea198bf939cf27c9b30c87000883c87b1854
Fittgross <william-gross@hotmail.com>
2024-01-28 14:39:13 +0100

    Added some error handling

--- ImageBorderTool/Program.cs
+++ ImageBorderTool/Program.cs
@@ -4,8 +4,18 @@
     {
         static void Main(string[] args)
         {
-            var app = new App();
-            app.Run();
+            if (args.Length > 0)
+            {
+                var app = new App();
+                app.Run(args);
+            }
+            else
+            {
+                Console.WriteLine("No images detected.");
+
+                Console.Write("\n\nPlease open the app with one or more images.\nPress any key to exit...");
+                Console.ReadKey();
+            }
         }
     }
 }
